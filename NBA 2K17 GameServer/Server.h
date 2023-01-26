f868537099989f790e34308ab3ba8f5cc96bea86
Botytec <106426289+Botytec@users.noreply.github.com>
2023-01-26 12:03:55 -0600

    Added Listen And Accept Connections

--- NBA 2K17 GameServer/Server.h
+++ NBA 2K17 GameServer/Server.h
@@ -16,6 +16,11 @@ public:
 	WORD ver = MAKEWORD(2, 2);
 	SOCKET MainSocket = socket(AF_INET, SOCK_STREAM, 0);
 	SOCKET ListenSocket = socket(AF_INET, SOCK_STREAM, 0);
+	int ClientSizes = sizeof(client);
+	SOCKET ClientSockets = accept(ClientSockets, (sockaddr*)&client, &ClientSizes);
+	SOCKADDR_IN main;
+	SOCKADDR_IN client;
+	bool bIsServerActive;
 
 	void StartServer()
 	{
@@ -26,8 +31,40 @@ public:
 		}
 	}
 
+	void StartListen()
+	{
+		main.sin_addr.S_un.S_addr = inet_addr("192.168.1.4");
+		main.sin_family = AF_INET;
+		main.sin_port = htons(17217);
 
+		ErrorCheck = bind(ListenSocket, (sockaddr*)&main, sizeof(main));
+		if (ErrorCheck != 0)
+		{
+			printf("[SERVER] Couldn't Bind Socket to main struct!");
+
+		}
+
+		ErrorCheck = listen(ListenSocket, SOMAXCONN);
+		if (ErrorCheck != 0)
+		{
+			printf("[SERVER] Couldn't listen for connectins!");
+
+
+		}
+		else
+		{
+			bIsServerActive = true;
+
+
+			while (bIsServerActive)
+			{
+				ClientSockets;
+
+			}
+		}
+
+
+
+	}
 
 };
-
-	
