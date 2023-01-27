66939841b16b92d7bcdd13cb27e9720a0d289ec3
Botytec <106426289+Botytec@users.noreply.github.com>
2023-01-27 11:31:15 -0600

    The offsets were for the park, these are for the game

--- NBA 2K17 GameServer/Offsets.h
+++ NBA 2K17 GameServer/Offsets.h
@@ -2,22 +2,27 @@
 namespace ONLINEBLACKTOP
 {
 
-	class COURT
+	class GAME_STATE_HANDLER
 	{
 	public:
 
 		void InitOffsets()
 		{
-			constexpr const char* UpdateState = "0x2C873C";
-			constexpr const char* StartStandardGame = "0x28C9A9";
-			constexpr const char* EndStandardGame = "0x1CD02C";
-
+			constexpr const char* SetGameState = "0x27F8AE";
+			constexpr const char* SetGameWaitingState = "0x27F95C";
+			constexpr const char* SetGameEndingState = "0x27F7FC";
+			constexpr const char* UpdateGameEnding = "0x2BA3AB";
+			constexpr const char* UpdateGamePlaying = "0x2BB40D";
+			constexpr const char* UpdateGameWaiting = "0x2BB5FF";
 
 
 		}
 
+	
 
 
 	};
 
+
+	claas 
 }
